19edbe91d5ab59cf2b6b30c212eff944db0cba79
cristianalazar <s222698@student.dtu.dk>
2023-06-09 22:05:05 +0200

    added unet

--- Project2_segmentation/network.py
+++ Project2_segmentation/network.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 class EncDec(nn.Module):
     def __init__(self):
@@ -43,4 +44,60 @@ class EncDec(nn.Module):
         d1 = F.relu(self.dec_conv1(self.upsample1(d0)))
         d2 = F.relu(self.dec_conv2(self.upsample2(d1)))
         d3 = self.dec_conv3(self.upsample3(d2))  # no activation
+        return d3
+    
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # encoder (downsampling)
+        self.enc_conv0 = nn.Conv2d(3, 64, 3, padding=1)
+        self.pool0 = nn.Conv2d(64, 64, 2, stride=2)  # 128 -> 64
+        self.enc_conv1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool1 = nn.Conv2d(128, 128, 2, stride=2)  # 64 -> 32
+        self.enc_conv2 = nn.Conv2d(128, 256, 3, padding=1)
+        self.pool2 = nn.Conv2d(256, 256, 2, stride=2)  # 32 -> 16
+        self.enc_conv3 = nn.Conv2d(256, 512, 3, padding=1)
+        self.pool3 = nn.Conv2d(512, 512, 2, stride=2)  # 16 -> 8
+
+        # bottleneck
+        self.bottleneck_conv = nn.Conv2d(512, 512, 3, padding=1)
+
+        # decoder (upsampling)
+        self.upsample0 = nn.ConvTranspose2d(512, 512, 2, stride=2)  # 8 -> 16
+        self.dec_conv0 = nn.Conv2d(512+512, 256, 3, padding=1)
+        self.upsample1 = nn.ConvTranspose2d(256, 256, 2, stride=2)  # 16 -> 32
+        self.dec_conv1 = nn.Conv2d(256+256, 128, 3, padding=1)
+        self.upsample2 = nn.ConvTranspose2d(128, 128, 2, stride=2)  # 32 -> 64
+        self.dec_conv2 = nn.Conv2d(128+128, 64, 3, padding=1)
+        self.upsample3 = nn.ConvTranspose2d(64, 64, 2, stride=2)  # 64 -> 128
+        self.dec_conv3 = nn.Conv2d(64+64, 1, 3, padding=1)
+
+    def forward(self, x):
+        # encoder
+        e0 = F.relu(self.enc_conv0(x))
+        p0 = self.pool0(e0)
+        e1 = F.relu(self.enc_conv1(p0))
+        p1 = self.pool1(e1)
+        e2 = F.relu(self.enc_conv2(p1))
+        p2 = self.pool2(e2)
+        e3 = F.relu(self.enc_conv3(p2))
+        p3 = self.pool3(e3)
+
+        # bottleneck
+        b = F.relu(self.bottleneck_conv(p3))
+        
+        # decoder
+        d0 = torch.cat([self.upsample0(b), e3], 1)
+        d0 = F.relu(self.dec_conv0(d0))
+        
+        d1 = torch.cat([self.upsample1(d0), e2], 1)
+        d1 = F.relu(self.dec_conv1(d1))
+        
+        d2 = torch.cat([self.upsample2(d1), e1], 1)
+        d2 = F.relu(self.dec_conv2(d2))
+        
+        d3 = torch.cat([self.upsample3(d2), e0], 1)
+        d3 = self.dec_conv3(d3)
         return d3
